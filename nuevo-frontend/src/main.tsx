abcf98611cee63eb309cf9c4465fdcd6e8aab4a9
Ester Pérez <esperi1@hotmail.es>
2025-06-10 19:39:04 +0200

    modificar archivo main.tsx

--- nuevo-frontend/src/main.tsx
+++ nuevo-frontend/src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';            // ← importa Provider
+import { store } from './app/store';                // ← importa store
 import DatosPersonalesPage from './pages/DatosPersonalesPage';
 import GameDashboardPage   from './pages/GameDashboardPage';
 import GameScenePage       from './pages/GameScenePage';
@@ -8,17 +10,21 @@ import UploadCVPage        from './pages/UploadCVPage';
 import ResultadosPage      from './pages/ResultadosPage';
 import './index.css';
 import './legacy.css';
-
+ 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/"            element={<DatosPersonalesPage />} />
-        <Route path="/games"       element={<GameDashboardPage />} />
-        <Route path="/games/:id"   element={<GameScenePage />} />
-        <Route path="/subircv"     element={<UploadCVPage />} />
-        <Route path="/resultados"  element={<ResultadosPage />} />
-      </Routes>
-    </BrowserRouter>
+-    <BrowserRouter>
++    <Provider store={store}>
++      <BrowserRouter>
+       <Routes>
+         <Route path="/"            element={<DatosPersonalesPage />} />
+         <Route path="/games"       element={<GameDashboardPage />} />
+         <Route path="/games/:id"   element={<GameScenePage />} />
+         <Route path="/subircv"     element={<UploadCVPage />} />
+         <Route path="/resultados"  element={<ResultadosPage />} />
+       </Routes>
+-    </BrowserRouter>
++      </BrowserRouter>
++    </Provider>
   </React.StrictMode>
 );
