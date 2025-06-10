0a392e782b0d3763e920d7659ba1c3da8da0cdce
Ester Pérez <esperi1@hotmail.es>
2025-06-10 21:46:32 +0200

    añadir líneas de código en main.tsx

--- nuevo-frontend/src/main.tsx
+++ nuevo-frontend/src/main.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
 import DatosPersonalesPage from './pages/DatosPersonalesPage';
 import GameDashboardPage   from './pages/GameDashboardPage';
 import GameScenePage       from './pages/GameScenePage';
 import UploadCVPage        from './pages/UploadCVPage';
 import ResultadosPage      from './pages/ResultadosPage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AccessibilitySettings } from "./components/AccessibilitySettings";
 import './index.css';
 import './legacy.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
+      <AccessibilitySettings />
       <BrowserRouter>
        <Routes>
          <Route path="/"            element={<DatosPersonalesPage />} />
