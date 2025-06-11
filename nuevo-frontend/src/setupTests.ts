57e2c6046ac87f7a6a6a31862993ac101cd72d6a
Ester Pérez <esperi1@hotmail.es>
2025-06-11 20:30:06 +0200

    añado código a setuptests

--- nuevo-frontend/src/setupTests.ts
+++ nuevo-frontend/src/setupTests.ts
@@ -1,1 +1,7 @@
-import '@testing-library/jest-dom'
+// src/setupTests.ts
+import '@testing-library/jest-dom'            // 1) importa los estilos y matchers
+import matchers from '@testing-library/jest-dom/matchers'
+import { expect } from 'vitest'
+
+// 2) registra los matchers de jest-dom en Vitest
+expect.extend(matchers)
