e7fb4063e7ae73f64f13a27dd1a5fc2bc6dc9b68
Ester Pérez <esperi1@hotmail.es>
2025-06-11 20:45:25 +0200

    añadir línea en preferencesstep.test

--- nuevo-frontend/src/features/personal/__tests__/PreferencesStep.test.tsx
+++ nuevo-frontend/src/features/personal/__tests__/PreferencesStep.test.tsx
@@ -1,3 +1,13 @@
+import '@testing-library/jest-dom';
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from '../../../app/store';
+import PreferencesStep from '../PreferencesStep';
+
+// …
+
 import React from 'react'
 import { render, screen, fireEvent } from '@testing-library/react'
 import { BrowserRouter } from 'react-router-dom'
