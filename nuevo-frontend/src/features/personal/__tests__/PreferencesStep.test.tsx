d241655eab76ff8e758be9d1fe5a5afd4150dc38
Ester Pérez <esperi1@hotmail.es>
2025-06-11 20:28:35 +0200

    corregir error en preferencesstep

--- nuevo-frontend/src/features/personal/__tests__/PreferencesStep.test.tsx
+++ nuevo-frontend/src/features/personal/__tests__/PreferencesStep.test.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { render, screen, fireEvent } from '@testing-library/react'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { store } from '../../app/store'
+import { store } from '../../../app/store'
 import PreferencesStep from '../PreferencesStep'
 
 describe('PreferencesStep', () => {
