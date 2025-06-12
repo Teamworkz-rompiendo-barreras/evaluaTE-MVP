4f51580a739e8ac46a4f4e4b514df54721f1a31c
Ester Pérez <esperi1@hotmail.es>
2025-06-12 17:31:57 +0200

    añado código en cypress.config

--- nuevo-frontend/cypress.config.ts
+++ nuevo-frontend/cypress.config.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from "cypress";
+// nuevo-frontend/cypress.config.ts
+import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    // carpeta donde están tus specs
+    specPattern: 'cypress/e2e/**/*.cy.{js,ts,jsx,tsx}',
+    // aquí apuntas al servidor de desarrollo
+    baseUrl: 'http://localhost:5173',
+    supportFile: false, // o 'cypress/support/index.ts' si tienes custom commands
   },
-});
+})
