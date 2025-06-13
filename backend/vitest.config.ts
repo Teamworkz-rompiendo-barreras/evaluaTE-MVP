672f30989937b60ed8a066286461d60846da8ef7
Ester Pérez <esperi1@hotmail.es>
2025-06-13 23:29:04 +0200

    corrijo código vitest.config.ts

--- backend/vitest.config.ts
+++ backend/vitest.config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    // Incluye cualquier .test.ts en el proyecto
     include: ['**/*.test.ts'],
-    exclude: ['node_modules', 'dist', 'cypress']
+    exclude: ['node_modules', 'dist']
   }
 })
+
