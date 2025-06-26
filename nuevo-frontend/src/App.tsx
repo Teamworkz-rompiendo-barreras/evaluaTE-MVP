7ae841666f79206426807f5f25cbe134b3345ed9
Ester Pérez <esperi1@hotmail.es>
2025-06-26 11:55:41 +0200

    mejoro código en app.tsx

--- nuevo-frontend/src/App.tsx
+++ nuevo-frontend/src/App.tsx
@@ -1,52 +1,30 @@
-// src/App.tsx
 import React from 'react'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-
-// Importa tus páginas y el guardia
 import DatosPersonalesPage from './pages/DatosPersonalesPage'
-import PreferencesStep      from './features/personal/PreferencesStep'
-import GameScenePage        from './pages/GameScenePage'
-import ProtectedRoute       from './components/ProtectedRoute'
-import { Toaster } from 'react-hot-toast'
+import PreferencesStep from './features/personal/PreferencesStep'
+import GameScenePage from './pages/GameScenePage'
+import GameDashboardPage from './pages/GameDashboardPage'
+import ProtectedRoute from './components/ProtectedRoute'
 import UploadCVPage from './pages/UploadCVPage'
+import { Toaster } from 'react-hot-toast'
+import ResultadosPage from './pages/ResultadosPage'
 
 export default function App() {
   return (
     <BrowserRouter>
-      {/* Toaster va aquí, fuera de <Routes> */}
       <Toaster position="top-center" />
       <Routes>
-        {/* Redirige la raíz al primer paso de registro */}
         <Route path="/" element={<Navigate to="/register/contact" replace />} />
-
-        {/* Paso 1: datos personales */}
-        <Route
-          path="/register/contact"
-          element={<DatosPersonalesPage />}
-        />
-
-        {/* Paso 2: preferencias laborales */}
-        <Route
-          path="/register/preferences"
-          element={<PreferencesStep />}
-        />
-
-        {/* RUTA PROTEGIDA: solo usuarios que han completado el registro y tienen progreso */}
-        <Route
-          path="/games/:id"
-          element={
-            <ProtectedRoute>
-              <GameScenePage />
-            </ProtectedRoute>
-          }
-        />
-        
-        <Route
-          path="/upload-cv"
-          element={<UploadCVPage />}
-        />
-
-        {/* Si la URL no coincide con nada, vuelve siempre al registro */}
+        <Route path="/register/contact" element={<DatosPersonalesPage />} />
+        <Route path="/register/preferences" element={<PreferencesStep />} />
+        <Route path="/games" element={<Navigate to="/games/1" replace />} />
+        <Route path="/games/:id" element={
+          <ProtectedRoute>
+            <GameScenePage />
+          </ProtectedRoute>
+        }/>
+        <Route path="/upload-cv" element={<UploadCVPage />} />
+        <Route path="/resultados" element={<ResultadosPage />} />
         <Route path="*" element={<Navigate to="/register/contact" replace />} />
       </Routes>
     </BrowserRouter>
