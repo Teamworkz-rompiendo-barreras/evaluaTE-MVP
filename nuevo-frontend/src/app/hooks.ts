e721d328ce280bbd8ac452096ab0752e53760520
Ester Pérez <esperi1@hotmail.es>
2025-06-27 20:19:11 +0200

    modifico hooks.ts

--- nuevo-frontend/src/app/hooks.ts
+++ nuevo-frontend/src/app/hooks.ts
@@ -2,6 +2,9 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 import type { RootState, AppDispatch } from './store'
 
-// Estos hooks tipados te evitan el “any” en useSelector y useDispatch
+// Exportamos RootState para poder usarlo en componentes
+export type { RootState } from './store'
+
+// Hooks tipados para evitar 'any'
 export const useAppDispatch = () => useDispatch<AppDispatch>()
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
