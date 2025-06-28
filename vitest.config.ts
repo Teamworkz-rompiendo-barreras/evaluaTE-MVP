1f3dc7fabe21e90f503b9d83e975028ea62582b6
Ester Pérez <esperi1@hotmail.es>
2025-06-28 21:13:31 +0200

    creo archivo .swaignore

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,15 +1,15 @@
-/// <reference types="vitest" />
-import { defineConfig } from 'vitest/config';
+// vitest.config.ts
+import { defineConfig } from 'vitest/config'
+
+// TypeScript ya no dará error al importar estos módulos
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  plugins: [react()],
   test: {
-    environment: 'jsdom',        // simulación de navegador
-    globals: true,               // permite usar expect() sin importarlo
-    setupFiles: './src/setupTests.ts', // <— aquí como array
-    include: [
-      'src/**/*.test.ts',
-      'src/**/*.test.tsx',
-      'src/**/__tests__/*.{test,spec}.{js,ts,tsx}'
-    ],
-  }
-});
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+    include: ['src/**/*.{test,spec}.{ts,tsx}'],
+    exclude: ['node_modules', 'dist']
+  },
+})
