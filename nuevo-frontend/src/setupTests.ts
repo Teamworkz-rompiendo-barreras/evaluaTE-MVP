448703b57d1e3ce7bb5cb8b4da4d28b640d6200f
Ester Pérez <esperi1@hotmail.es>
2025-06-29 19:14:52 +0200

    modifico setuptests

--- nuevo-frontend/src/setupTests.ts
+++ nuevo-frontend/src/setupTests.ts
@@ -3,6 +3,17 @@
 import '@testing-library/jest-dom/extend-expect'
 import { server } from './mocks/server'
 
-beforeAll(() => server.listen())
-afterEach(() => server.resetHandlers())
-afterAll(() => server.close())
+// Inicia el servidor mock antes de todas las pruebas
+beforeAll(() => {
+  server.listen()
+})
+
+// Reinicia los handlers después de cada prueba
+afterEach(() => {
+  server.resetHandlers()
+})
+
+// Detiene el servidor al finalizar
+afterAll(() => {
+  server.close()
+})
