3716a70513a44e9636d32c61294c5c95e5d7e1e9
Ester Pérez <esperi1@hotmail.es>
2025-06-29 20:10:07 +0200

    creo archivo msw.d.ts

---
+++ nuevo-frontend/src/types/msw.d.ts
@@ -0,0 +1,7 @@
+// src/types/msw.d.ts
+declare module 'msw' {
+  export function rest(): any
+  export type RestRequest = any
+  export type ResponseResolver = any
+  export type RestContext = any
+}
