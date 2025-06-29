c671e856466268f9a3452b3a0e33a136581a1d07
Ester Pérez <esperi1@hotmail.es>
2025-06-29 12:02:28 +0200

    modifico archivo vite-plugin-react

--- nuevo-frontend/src/types/vite-plugin-react.d.ts
+++ nuevo-frontend/src/types/vite-plugin-react.d.ts
@@ -1,5 +1,7 @@
 // src/types/vite-plugin-react.d.ts
+import type { Plugin } from 'rollup'
+
 declare module '@vitejs/plugin-react' {
-  import type { Plugin } from 'rollup'
-  export default function react(): Plugin
+  const react: () => Plugin
+  export default react
 }
