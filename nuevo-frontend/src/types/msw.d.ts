792b881ff2b303494f7ec11f5dbbdff5b874c8f6
Ester Pérez <esperi1@hotmail.es>
2025-07-01 11:28:48 +0200

    corrigo msw.d.ts

--- nuevo-frontend/src/types/msw.d.ts
+++ nuevo-frontend/src/types/msw.d.ts
@@ -1,7 +1,20 @@
 // src/types/msw.d.ts
+import type { RequestHandler, RestContext, RestRequest } from 'msw'
 declare module 'msw' {
   export function rest(): any
   export type RestRequest = any
   export type ResponseResolver = any
   export type RestContext = any
+  export const rest: {
+    get: (path: string, resolver: RequestHandler) => void
+    post: (path: string, resolver: RequestHandler) => void
+    put: (path: string, resolver: RequestHandler) => void
+    delete: (path: string, resolver: RequestHandler) => void
+  }
+
+  export function setupServer(...handlers: RequestHandler[]): {
+    listen: () => void
+    close: () => void
+    resetHandlers: () => void
+  }
 }
