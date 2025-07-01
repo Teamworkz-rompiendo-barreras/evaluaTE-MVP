4e1633009710751d9eec7485b6664086ac145ab8
Ester Pérez <esperi1@hotmail.es>
2025-07-01 14:44:50 +0200

    modifico preferences.step.fixture

--- nuevo-frontend/src/features/personal/__tests__/preferences.step.fixture.ts
+++ nuevo-frontend/src/features/personal/__tests__/preferences.step.fixture.ts
@@ -1,12 +1,15 @@
 // src/features/personal/__tests__/preferences.step.fixture.ts
+
+import type { JobPreference } from '@/types/preferences'
 import { mockUser } from '@/__fixtures__/user.fixtures'
 
-const preferencesFixture = {
-  jobPreferences: mockUser.jobPreferences,
-  workMode: mockUser.jobPreferences.workMode,
-  availability: mockUser.jobPreferences.availability,
-  willingToRelocate: mockUser.jobPreferences.willingToRelocate,
-  hasDisabilityCert: mockUser.jobPreferences.hasDisabilityCert
+const preferencesFixture: JobPreference = {
+  areas: mockUser.jobPreferences.areas || ['Desarrollo web'],
+  needs: mockUser.jobPreferences.needs || ['Horario flexible', 'Acceso remoto'],
+  workMode: mockUser.jobPreferences.workMode || 'remoto',
+  availability: mockUser.jobPreferences.availability || 'mañana',
+  willingToRelocate: mockUser.jobPreferences.willingToRelocate || false,
+  hasDisabilityCert: mockUser.jobPreferences.hasDisabilityCert || true,
 }
 
 export default preferencesFixture
