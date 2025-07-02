3b9167b008bd9d822b99ec1aaaf8e840e37348df
Ester Pérez <esperi1@hotmail.es>
2025-07-02 11:46:22 +0200

    creo vitest.config.ts

---
+++ nuevo-frontend/vitest.config.ts
@@ -0,0 +1,9 @@
+// nuevo-frontend/vitest.config.ts
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    environment: 'jsdom',
+    globals: true,
+  },
+});
