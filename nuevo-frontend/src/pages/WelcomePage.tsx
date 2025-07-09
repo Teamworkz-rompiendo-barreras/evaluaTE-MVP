135a7cce7735ece2eb14821f5ff7989eef092d3f
Ester Pérez <esperi1@hotmail.es>
2025-07-09 21:17:26 +0200

    modifico archivo welcome

--- nuevo-frontend/src/pages/WelcomePage.tsx
+++ nuevo-frontend/src/pages/WelcomePage.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const WelcomePage: React.FC = () => {
   const navigate = useNavigate();
+
+  useEffect(() => {
+    console.log('WelcomePage MONTADO');
+    return () => {
+      console.log('WelcomePage DESMONTADO');
+    };
+  }, []);
 
   const handleStart = () => {
     localStorage.setItem('welcomeSeen', 'true');
